80cdfa86b00a4bc14c443e1c500f5a6b41a1a0e9
prajapati-pallavi <nielit.pallavi@gmail.com>
2026-03-20 10:57:50 +0530

    Initial commit - Docker Compose project

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import mysql.connector
+import time
+
+app = Flask(__name__)
+
+def connect_db():
+    time.sleep(5)  # wait for MySQL to start
+    return mysql.connector.connect(
+        host="db",
+        user="root",
+        password="root",
+        database="testdb"
+    )
+
+@app.route('/')
+def home():
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("SELECT DATABASE();")
+        result = cursor.fetchone()
+        return f"Connected to MySQL: {result}"
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
